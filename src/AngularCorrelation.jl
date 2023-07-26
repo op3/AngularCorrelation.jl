0a382583a591ddda2f925ce1cc520e05b0d84ade
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2023-07-26 15:42:21 +0200

    Split coefficient calculation for better vectorized performance

--- src/AngularCorrelation.jl
+++ src/AngularCorrelation.jl
@@ -1,8 +1,8 @@
 module AngularCorrelation
 
-export W, Wcorr, Parity, EMCharacter, State, Transition, Dipole, Quadrupole, E1, M1, E2, M2
+export W, Wcorr, W_coeff, Wcorr_coeff, Parity, EMCharacter, State, Transition, Dipole, Quadrupole, E1, M1, E2, M2
 
-using SphericalHarmonics: computeYlm, computePlmcostheta, Unnormalized, associatedLegendre
+using SphericalHarmonics: Unnormalized, associatedLegendre, sphericalharmonic
 
 include("coefficients.jl")
 
@@ -22,6 +22,74 @@ function final_state_orientation(λ2, cascade...)
     res * A(λ2, cascade[end-2], cascade[end-1], cascade[end])
 end
 
+function Wcorr_coeff(
+    S0::State, γ0::Transition,
+    S1::State, γ1::Transition,
+    cascade...)
+    coeff = Dict{Vector{Int8},Float64}()
+    for λ0 in 0:2:4
+        for λ1 in 0:2:4
+            for λ2 in 0:2:4
+                pre = (
+                    (-1)^(λ1 + λ2) /
+                    (√(2 * λ2 + 1)) *
+                    A(λ1, λ2, λ0, S1, γ1, cascade[begin]) *
+                    final_state_orientation(λ2, cascade...)
+                )
+                for q0 in range(-λ0, λ0, step=2)
+                    orientation = B_lpol(q0, λ0, S0, γ0, S1)
+                    for q1 in range(-λ1, λ1, step=2)
+                        for q2 in range(-λ2, λ2, step=2)
+                            val = (
+                                pre * orientation *
+                                wigner3j(λ2, λ1, λ0, q2, q1, q0)
+                            )
+                            if val != 0.0
+                                if !([λ1, λ2, q1, q2] in keys(coeff))
+                                    coeff[[λ1, λ2, q1, q2]] = 0.0
+                                end
+                                coeff[[λ1, λ2, q1, q2]] += (
+                                    pre * orientation *
+                                    wigner3j(λ2, λ1, λ0, q2, q1, q0)
+                                )
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return coeff
+end
+
+function Wcorr(
+    theta1::AbstractFloat, phi1::AbstractFloat,
+    theta2::AbstractFloat, phi2::AbstractFloat,
+    coeff::Dict{Vector{Int8},Float64})
+    res = 0.0
+    for ((λ1, λ2, q1, q2), c) in coeff
+        res += c *
+               sphericalharmonic(theta1, phi1, λ1, q1) *
+               sphericalharmonic(theta2, phi2, λ2, q2)
+    end
+    # for λ1 in 0:2:4
+    #     for λ2 in 0:2:4
+    #         for q1 in range(-λ1, λ1, step=2)
+    #             for q2 in range(-λ2, λ2, step=2)
+    #                 if coeff[[λ1, λ2, q1, q2]] != 0.0
+    #                     res += (
+    #                         coeff[[λ1, λ2, q1, q2]] *
+    #                         sphericalharmonic(theta1, phi1, λ1, q1) *
+    #                         sphericalharmonic(theta2, phi2, λ2, q2)
+    #                     )
+    #                 end
+    #             end
+    #         end
+    #     end
+    # end
+    real(res)
+end
+
 """
 Angular correlation for a triple γ cascade
 
@@ -39,35 +107,8 @@ function Wcorr(
     S0::State, γ0::Transition,
     S1::State, γ1::Transition,
     cascade...)
-    res = 0
-    Ynm_1 = computeYlm(theta1, phi1, lmax=4)
-    Ynm_2 = computeYlm(theta2, phi2, lmax=4)
-    for λ0 in 0:2:4
-        for λ1 in 0:2:4
-            for λ2 in 0:2:4
-                pre = (
-                    (-1)^(λ1 + λ2) /
-                    (√(2 * λ2 + 1)) *
-                    A(λ1, λ2, λ0, S1, γ1, cascade[begin]) *
-                    final_state_orientation(λ2, cascade...)
-                )
-                for q0 in range(-λ0, λ0, step=2)
-                    orientation = B_lpol(q0, λ0, S0, γ0, S1)
-                    for q1 in range(-λ1, λ1, step=2)
-                        for q2 in range(-λ2, λ2, step=2)
-                            res += (
-                                pre * orientation *
-                                wigner3j(λ2, λ1, λ0, q2, q1, q0) *
-                                Ynm_1[(λ1, q1)] *
-                                Ynm_2[(λ2, q2)]
-                            )
-                        end
-                    end
-                end
-            end
-        end
-    end
-    real(res)
+    coeff = Wcorr_coeff(S0, γ0, S1, γ1, cascade...)
+    Wcorr(theta1, phi1, theta2, phi2, coeff)
 end
 
 precompile(Wcorr, (Float64, Float64, Float64, Float64, State, Transition, State, Transition, State, Transition, State))
@@ -78,6 +119,22 @@ precompile(Wcorr, (BigFloat, BigFloat, BigFloat, BigFloat, State, Transition, St
 precompile(Wcorr, (BigFloat, BigFloat, BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State))
 precompile(Wcorr, (BigFloat, BigFloat, BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
 precompile(Wcorr, (BigFloat, BigFloat, BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
+
+function W_coeff(
+    S0::State, γ0::Transition,
+    cascade...)
+    coeff_Pl0 = Dict{Int8,Float64}()
+    coeff_Pl2 = Dict{Int8,Float64}()
+    for λ in 0:2:4
+        fso = final_state_orientation(λ, cascade...)
+        coeff_Pl0[λ] = B(λ, S0, γ0, cascade[begin]) * fso
+        if λ ≥ 2
+            coeff_Pl2[λ] =
+                B_lpol(λ, S0, γ0, cascade[begin]) * fso
+        end
+    end
+    return coeff_Pl0, coeff_Pl2
+end
 
 """
 Angular distribution for a single emitted γ
@@ -95,28 +152,24 @@ function W(
     S0::State, γ0::Transition,
     cascade...)
     res = 0
-    # Pl0costheta = computePlmcostheta(theta, lmax=4, m=0, norm=Unnormalized())
-    # Pl2costheta = computePlmcostheta(theta, lmax=4, m=2, norm=Unnormalized())
+    coeff_Pl0, coeff_Pl2 = W_coeff(S0, γ0, cascade...)
+    c2phi = cos(2 * phi)
     for λ in 0:2:4
-        res += (
-            B(λ, S0, γ0, cascade[begin]) * associatedLegendre(theta, l=λ, m=0, norm=Unnormalized()) +
-            if λ ≥ 2
-                B_lpol(λ, S0, γ0, cascade[begin]) * associatedLegendre(theta, l=λ, m=2, norm=Unnormalized()) * cos(2 * phi)
-            else
-                0
-            end
-        ) * final_state_orientation(λ, cascade...)
+        res += coeff_Pl0[λ] * associatedLegendre(theta, l=λ, m=0, norm=Unnormalized())
+    end
+    for λ in 2:2:4
+        res += coeff_Pl2[λ] * associatedLegendre(theta, l=λ, m=2, norm=Unnormalized()) * c2phi
     end
     return res
 end
 
-precompile(Wcorr, (Float64, Float64, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (Float64, Float64, State, Transition, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (Float64, Float64, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (Float64, Float64, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
-precompile(Wcorr, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (Float64, Float64, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (Float64, Float64, State, Transition, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (Float64, Float64, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (Float64, Float64, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
+precompile(W, (BigFloat, BigFloat, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State, Transition, State))
 
 end # module AngularCorrelation
